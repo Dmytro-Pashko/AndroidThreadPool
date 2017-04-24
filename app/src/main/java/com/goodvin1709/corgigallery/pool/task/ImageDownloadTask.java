bcb8570f6ba979cd043f8df6642de1bcc48e6ac9
D.Pashko <Goodvin1709@gmai.com>
2017-04-24 12:31:27 +0300

    Added connection time out for download image task

--- app/src/main/java/com/goodvin1709/corgigallery/pool/task/ImageDownloadTask.java
+++ app/src/main/java/com/goodvin1709/corgigallery/pool/task/ImageDownloadTask.java
@@ -8,9 +8,11 @@ import com.goodvin1709.corgigallery.model.Image;
 
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class ImageDownloadTask implements Runnable {
 
+    private static final int CONNECTION_TIMEOUT = 10000;
     private Image image;
     private DownloadListener handler;
 
@@ -20,7 +22,10 @@ public class ImageDownloadTask implements Runnable {
     }
 
     private void downloadImage() throws IOException {
-        Bitmap bitmap = BitmapFactory.decodeStream(new URL(image.getUrl()).openStream());
+        URL url = new URL(image.getUrl());
+        URLConnection connection = url.openConnection();
+        connection.setConnectTimeout(CONNECTION_TIMEOUT);
+        Bitmap bitmap = BitmapFactory.decodeStream(connection.getInputStream());
         handler.onImageDownloaded(image, bitmap);
     }
 
